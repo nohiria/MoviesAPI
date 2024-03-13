e186527961c5005c4ad956578317008c9eb9c2cb
Nohiria Velazquez <nohiriavg@gmail.com>
2024-03-12 19:20:28 -0500

    Creating Movie and Review classes

---
+++ src/main/java/dev/nohiria/movies/Movie.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dev.nohiria.movies;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+/**
+ *
+ * @author nohyv
+ */
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    @DocumentReference
+    private List<Review> reviewIds; 
+}
---
+++ src/main/java/dev/nohiria/movies/Review.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dev.nohiria.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ *
+ * @author nohyv
+ */
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    private ObjectId id;
+    private String body;
+}
