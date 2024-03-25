abf652f78ed3c4612f496f52e527f6e1b5ee0c60
Nohiria Velazquez <nohiriavg@gmail.com>
2024-03-25 13:33:32 -0500

    Adding CorsConfig

---
+++ src/main/java/dev/nohiria/movies/Config/CorsConfig.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dev.nohiria.movies.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author nohyv
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow CORS for all routes
+                .allowedOrigins("http://localhost:3000") // Allow requests from any origin
+                .allowedMethods("GET", "POST", "PUT") // Allow specified HTTP methods
+                .allowedHeaders("*"); // Allow all headers
+    }
+}
